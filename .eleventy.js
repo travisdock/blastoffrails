b96703fcd47fa7c3fdb8642bba557655aa928d1e
Name <email>
2025-07-03 19:12:01 -0600

    Add WebC and use that for cleaning things up a bit

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,9 @@
+const pluginWebc = require("@11ty/eleventy-plugin-webc");
+
 module.exports = function(eleventyConfig) {
+  eleventyConfig.addPlugin(pluginWebc, {
+    components: "src/_components/**/*.webc"
+  });
   eleventyConfig.addPassthroughCopy("css");
   eleventyConfig.addPassthroughCopy("src/CNAME");
   
