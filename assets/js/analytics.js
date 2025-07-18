eb4021458e89b8af22f2c2cd4cf44961d9e4605f
Travis Dockter <travisdock@gmail.com>
2025-07-17 22:32:34 -0700

    Add analytics

---
+++ assets/js/analytics.js
@@ -0,0 +1,9 @@
+// Travalytics Analytics
+(function() {
+  const script = document.createElement('script');
+  script.src = 'https://travserve.net/analytics.js';
+  script.setAttribute('data-tracking-id', '0543527a-695b-488a-8152-1341efef043f');
+  script.setAttribute('data-endpoint', 'https://travserve.net');
+  script.async = true;
+  document.head.appendChild(script);
+})();
