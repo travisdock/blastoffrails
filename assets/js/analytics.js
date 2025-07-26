afbef506aba780650a3e3ad24e667b6d5c9d3d13
Travis Dockter <travisdock@gmail.com>
2025-07-26 07:52:22 -0600

    Don't track localhost

--- assets/js/analytics.js
+++ assets/js/analytics.js
@@ -1,5 +1,13 @@
 // Travalytics Analytics
 (function() {
+  if (window.location.hostname === 'localhost' || 
+      window.location.hostname === '127.0.0.1' || 
+      window.location.hostname.startsWith('192.168.') ||
+      window.location.hostname.startsWith('10.') ||
+      window.location.hostname === '::1') {
+    console.log('Travalytics: Skipping tracking on localhost');
+    return;
+  }
   const script = document.createElement('script');
   script.src = 'https://travserve.net/analytics.js';
   script.setAttribute('data-tracking-id', '0543527a-695b-488a-8152-1341efef043f');
